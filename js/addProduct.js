3f60e0585b0f797b860cde1b81adf519f9228f95
trancohung2001 <trancohung2016@gmail.com>
2024-12-14 00:30:03 +0700

    addProducts

---
+++ js/addProduct.js
@@ -0,0 +1,66 @@
+function getProduct() {
+    return JSON.parse(localStorage.getItem("products")) || [];
+}
+
+function saveProducts(products) {
+    localStorage.setItem("products", JSON.stringify(products));
+}
+
+function addProduct(image, name, price, discount) {
+    if (!image || !name || !price || !discount) {
+        alert("Xin đừng bỏ trống!");
+    }
+
+    const finalPrice = price * (1 - discount /100);
+    const products = getProduct();
+    products.push({ image, name, price, discount, finalPrice});
+    saveProducts(products);
+    renderProducts();
+}
+
+
+function renderProducts() {
+    const productList = document.getElementById("productList");
+    productList.innerHTML = "";
+    const products = getProduct();
+
+    products.forEach((product, index) => {
+        const productDiv = document.createElement("div");
+        productDiv.className = "border p-4 rounded shadow";
+
+        productDiv.innerHTML = `
+            <img src="${product.image}" alt="${product.name}" class="w-full h-60 object-cover mb-4 rounded">
+            <h2 class="text-lg font-bold">${product.name}</h2>
+            <p class="text-gray-600">${product.price} VND</p>
+            <p class="text-gray-600">${product.finalPrice} VND</p>
+            <div class="flex gap-4">
+                <button class="bg-[#DB4444] rounded w-full p-2 mt-2 text-white">
+                    <a href="#">Xem chi tiết sản phẩm</a>
+                </button>
+                <button class="bg-[#DB4444] rounded w-full p-2 mt-2 text-white" onclick="deleteProduct(${index})">Xóa</button>
+            </div>
+        `;
+        productList.appendChild(productDiv);
+    })
+}
+
+function deleteProduct(index) {
+    const products = getProduct();
+    products.splice(index, 1);
+    saveProducts(products);
+    renderProducts();
+}
+
+document.getElementById("addProductBtn").addEventListener("click", () => {
+    const image = document.getElementById("productImage").value;
+    const name = document.getElementById("productName").value;
+    const price = document.getElementById("productPrice").value;
+    const discount = document.getElementById("productDiscount").value;
+    addProduct(image, name, price, discount);
+    document.getElementById("productImage").value = "";
+    document.getElementById("productName").value = "";
+    document.getElementById("productPrice").value = "";
+    document.getElementById("productDiscount").value = "";
+})
+
+renderProducts();
--- js/index.js
+++ js/index.js
@@ -26,3 +26,34 @@ if (userLogin) {
 } else {
   userLoginElement.innerHTML = "";
 }
+
+function getProduct() {
+  return JSON.parse(localStorage.getItem("products")) || [];
+}
+
+function renderProducts() {
+  const productList = document.querySelector("#products .items");
+  productList.innerHTML = "";
+
+  const products = getProduct();
+
+  products.forEach((product) => {
+    const productDiv = document.createElement("div");
+    productDiv.className = "border p-4 rounded shadow";
+
+    productDiv.innerHTML = `
+      <div class="border rounded">
+        <img src="${product.image}" alt="${product.name}" class="w-full h-60 object-cover mb-4 rounded"/>
+      </div>
+      <h2 class="text-lg font-bold">${product.name}</h2>
+      <div class="item-price">
+        <span class="text-gray-600">${product.discount}</span>
+        <span class="text-gray-600">${product.price}</span>
+      </div>
+      <button class="btn bg-[#DB4444] rounded w-full p-2 mt-2 text-white">Add to my Cart</button>
+    `;
+    productList.appendChild(productDiv);
+  });
+}
+
+renderProducts();
